2ec52aae68b205a4a7bc8524dfd032375d5badf3
Yuta Nakamura <nakamurayuta0204@gmail.com>
2024-10-19 20:26:24 +0900

    Add comment

--- src/Lib/HTML.hs
+++ src/Lib/HTML.hs
@@ -1,57 +1,65 @@
-module Lib.HTML (openTag, closeTag,element,quotedText,html,Node(Element, Text)  ) where
-import Text.ParserCombinators.Parsec
+module Lib.HTML (openTag, closeTag, element, quotedText, html, comment, Node (Element, Text)) where
 
+import Text.ParserCombinators.Parsec
 
 openTag :: GenParser Char st (String, [Attribute])
 openTag = do
-    char '<'
-    tagName <- many letter
-    attributes <- try (char ' ' >> sepBy attribute (char ' ')) <|> return []
-    char '>'
-    return (tagName,attributes)
+  char '<'
+  tagName <- many letter
+  attributes <- try (char ' ' >> sepBy attribute (char ' ')) <|> return []
+  char '>'
+  return (tagName, attributes)
 
 attribute :: GenParser Char st Attribute
 attribute = do
-    key <- many letter
-    char '='
-    value <- quotedText
-    return (key, value)
+  key <- many letter
+  char '='
+  value <- quotedText
+  return (key, value)
 
 quotedText :: GenParser Char st String
 quotedText = do
-    char '"'
-    text <- many (escapeChar <|> noneOf "\"\\")
-    char '"'
-    return text
+  char '"'
+  text <- many (escapeChar <|> noneOf "\"\\")
+  char '"'
+  return text
 
 escapeChar :: GenParser Char st Char
 escapeChar = do
-    try (string "\\\"" >> return '"')
-
+  try (string "\\\"" >> return '"')
 
 closeTag :: GenParser Char st String
 closeTag = do
-    string "</"
-    content <- many (noneOf "<>")
-    char '>'
-    return content
+  string "</"
+  content <- many (noneOf "<>")
+  char '>'
+  return content
+
+comment :: GenParser Char st String
+comment = between (string "<!--") (string "-->") (many (noneOf "<!-"))
 
 textNode :: GenParser Char st Node
 textNode = do
-    text <- many1 (noneOf "<>")
-    return $ Text text
+  text <- many1 (noneOf "<>")
+  return $ Text text
+
+singleElement :: GenParser Char st Node
+singleElement = do
+  (tagName, at) <- openTag
+  return $ Element tagName at []
 
 element :: GenParser Char st Node
 element = do
-    (tag, attributes) <- openTag
-    nodes <- many (try element <|> textNode)
-    closingTag <- closeTag
-    if tag == closingTag
-        then return (Element tag attributes nodes)
-        else fail $ "Expected closing tag </" ++ tag ++ ">, but got </" ++ closingTag ++ ">"
+  (tag, attributes) <- openTag
+  nodes <- many (try element <|> try singleElement <|> try (comment >> return (Text "")) <|> textNode)
+  closingTag <- closeTag
+  if tag == closingTag
+    then return (Element tag attributes nodes)
+    else fail $ "Expected closing tag </" ++ tag ++ ">, but got </" ++ closingTag ++ ">"
 
 html :: GenParser Char st [Node]
 html = many element
 
-type Attribute = (String,String)
+type Attribute = (String, String)
+
 data Node = Element String [Attribute] [Node] | Text String deriving (Show, Eq)
--- test/Lib/HTMLTest.hs
+++ test/Lib/HTMLTest.hs
@@ -1,53 +1,95 @@
 module Lib.HTMLTest (htmlTests) where
 
-import Test.HUnit
 import Lib.HTML
+import Test.HUnit
 import Text.ParserCombinators.Parsec
 
-
 testOpenTag :: Test
-testOpenTag = TestCase (do
-    let input = "<body id=\"test\">"
-    let result = parse openTag "" input
-    let exepected = Right ("body",[("id", "test")])
-    assertEqual "parse openTag"  exepected result
+testOpenTag =
+  TestCase
+    ( do
+        let input = "<body id=\"test\">"
+        let result = parse openTag "" input
+        let exepected = Right ("body", [("id", "test")])
+        assertEqual "parse openTag" exepected result
     )
 
 testQuotedText :: Test
-testQuotedText = TestCase (do
-    let input = "\"abc\""
-    let result = parse quotedText "" input
-    assertEqual "parse quotedText" (Right "abc") result
+testQuotedText =
+  TestCase
+    ( do
+        let input = "\"abc\""
+        let result = parse quotedText "" input
+        assertEqual "parse quotedText" (Right "abc") result
+    )
+
+testComment :: Test
+testComment =
+  TestCase
+    ( do
+        let input = "<!--comment-->"
+        let result = parse comment "" input
+        assertEqual "parse quotedText" (Right "comment") result
     )
 
 testQuotedTextWithEscape :: Test
-testQuotedTextWithEscape = TestCase (do
-    let input = "\"\\\"\""
-    let result = parse quotedText "" input
-    assertEqual "parse quotedText" (Right "\"") result)
-
+testQuotedTextWithEscape =
+  TestCase
+    ( do
+        let input = "\"\\\"\""
+        let result = parse quotedText "" input
+        assertEqual "parse quotedText" (Right "\"") result
+    )
 
 testElement1 :: Test
-testElement1 = TestCase (do
-    let input = "<div>Hello, World!</div>"
-    let expected = Right (Element "div" [] [Text "Hello, World!"])
-    let result = parse element "" input
-    assertEqual "Parsing <div>Hello, World!</div>" expected result)
+testElement1 =
+  TestCase
+    ( do
+        let input = "<div>Hello, World!</div>"
+        let expected = Right (Element "div" [] [Text "Hello, World!"])
+        let result = parse element "" input
+        assertEqual "Parsing <div>Hello, World!</div>" expected result
+    )
 
 testNested :: Test
-testNested = TestCase (do
-    let input = "<html><body></body></html>"
-    let expected = Right (Element "html" [] [Element "body" [] []])
-    let result = parse element "" input
-    assertEqual "Parsing nested document" expected result)
+testNested =
+  TestCase
+    ( do
+        let input = "<html><body></body></html>"
+        let expected = Right (Element "html" [] [Element "body" [] []])
+        let result = parse element "" input
+        assertEqual "Parsing nested document" expected result
+    )
 
 testNested2 :: Test
-testNested2 = TestCase (do
-    let input = "<div><div></div></div>"
-    let expected = Right (Element "div" [] [Element "div" [] []])
-    let result = parse element "" input
-    assertEqual "Parsing nested document" expected result)
+testNested2 =
+  TestCase
+    ( do
+        let input = "<div><div></div></div>"
+        let expected = Right (Element "div" [] [Element "div" [] []])
+        let result = parse element "" input
+        assertEqual "Parsing nested document" expected result
+    )
 
+testSingleTag :: Test
+testSingleTag =
+  TestCase
+    ( do
+        let input = "<div>a<br>b</div>"
+        let expected = Right (Element "div" [] [Text "a", Element "br" [] [], Text "b"])
+        let result = parse element "" input
+        assertEqual "Parsing element with single tag" expected result
+    )
+
+testWithComment :: Test
+testWithComment =
+  TestCase
+    ( do
+        let input = "<div><!--comment-->a</div>"
+        let expected = Right (Element "div" [] [Text "", Text "a"])
+        let result = parse element "" input
+        assertEqual "Parsing element with comment" expected result
+    )
 
 htmlTests :: Test
-htmlTests = TestList [testOpenTag,testQuotedText,testQuotedTextWithEscape,testElement1,testNested,testNested2]
+htmlTests = TestList [testOpenTag, testQuotedText, testQuotedTextWithEscape, testElement1, testNested, testComment, testWithComment, testSingleTag, testNested2]
