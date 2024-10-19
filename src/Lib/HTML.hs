3fdccf8aab175e24346e467b7a66fdbfb88f4000
Yuta Nakamura <nakamurayuta0204@gmail.com>
2024-10-20 00:24:06 +0900

    improved single node parsing

--- src/Lib/HTML.hs
+++ src/Lib/HTML.hs
@@ -2,18 +2,19 @@ module Lib.HTML (openTag, closeTag, element, quotedText, html, comment, Node (El
 
 import Text.ParserCombinators.Parsec
 import Text.Parsec (parserTrace)
+import Debug.Trace (trace, traceId)
 
 openTag :: GenParser Char st (String, [Attribute])
 openTag = do
   char '<'
-  tagName <- many letter
+  tagName <- many1 letter
   attributes <- try (char ' ' >> sepBy attribute (char ' ')) <|> return []
   char '>'
   return (tagName, attributes)
 
 attribute :: GenParser Char st Attribute
 attribute = do
-  key <- many letter
+  key <- many1 letter
   char '='
   value <- quotedText
   return (key, value)
@@ -46,15 +47,27 @@ textNode = do
   text <- many1 (noneOf "<>")
   return $ Text text
 
-singleElement :: GenParser Char st Node
-singleElement = do
-  (tagName, at) <- openTag
-  return $ Element tagName at []
+voidTags = ["area", "base", "br", "col", "embed", "hr", "img", "input", "link", "meta", "param", "source", "track", "wbr"]
+
+voidElement :: GenParser Char st Node
+voidElement = do
+  char '<'
+  tagName <- choice $ map (try . string) voidTags
+  attributes <- try (char ' ' >> sepBy attribute (char ' ')) <|> return []
+  char '>'
+  return $ Element tagName attributes []
+
+parseNode ::GenParser Char st Node
+parseNode = do
+  parserTrace "parseNode"
+  choice [try voidElement, try element, try (comment >> return (Text "")), textNode]
 
 element :: GenParser Char st Node
 element = do
   (tag, attributes) <- openTag
-  nodes <- many (try element <|> try singleElement <|> try (comment >> return (Text "")) <|> textNode)
+  _ <- trace ("\n ===tag: " ++ tag ++ "===\n") (return ())
+  nodes <- many parseNode
+  _ <- trace ("\n ===Node ("++ tag ++") Parsed: " ++ show nodes ++ "===\n") (return ())
   closingTag <- closeTag
   if tag == closingTag
     then return (Element tag attributes nodes)
