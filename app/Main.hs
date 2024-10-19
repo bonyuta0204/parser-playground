ecc6ab2c9a0b3f661a09b64e0a7431eeeeb0ce7d
Yuta Nakamura <nakamurayuta0204@gmail.com>
2024-10-19 10:17:46 +0900

    shorter parser

--- app/Main.hs
+++ app/Main.hs
@@ -7,33 +7,17 @@ main :: IO ()
 main = someFunc
 
 csvFile :: GenParser Char st [[String]]
-csvFile = do
-  result <- many line
-  eof
-  return result
+csvFile = endBy line eol
+
 
 line :: GenParser Char st [String]
-line = do
-  result <- cells
-  eol
-  return result
+line = sepBy cell (char ',')
 
-cells :: GenParser Char st [String]
-cells = do
-  first <- cellContent
-  next <- remainingCells
-  return (first : next)
-
-remainingCells :: GenParser Char st [String]
-remainingCells =
-  (char ',' >> cells)
-  <|> return []
-
-cellContent :: GenParser Char st String
-cellContent = many (noneOf ",\n")
+cell :: GenParser Char st String 
+cell = many (noneOf "\n,")
 
 eol :: GenParser Char st Char
 eol = char '\n'
 
 parseCSV :: String -> Either ParseError [[String]]
-parseCSV input = parse csvFile "(unknown)" input
+parseCSV = parse csvFile "(unknown)"
