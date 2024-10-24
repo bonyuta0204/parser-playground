55557a50df2425bd3fb78a5566d3380d0c4d51f0
Yuta Nakamura <nakamurayuta0204@gmail.com>
2024-10-24 22:56:06 +0900

    add rational calculation

--- src/Lib/Calc.hs
+++ src/Lib/Calc.hs
@@ -2,105 +2,113 @@ module Lib.Calc (expression, calculator, Expression (Val, Sum, Mul, Sub, Div)) w
 
 import Text.ParserCombinators.Parsec
 
-val :: Parser Expression
-val = do
-  num <- many1 digit
-  return $ Val (read num :: Int)
-
-termSum :: Parser Expression
-termSum = do
-  a <- termFactor
-  spaces
-  rest a
-  where
-    rest left =
-      ( do
-          s <- char '+' <|> char '-'
-          spaces
-          b <- termFactor
-          spaces
-          rest $ case s of
-            '+' -> Sum left b
-            '-' -> Sub left b
-            _ -> b
-      )
-        <|> return left
-
-termFactor :: Parser Expression
-termFactor = do
-  a <- subExpression <|> val
-  spaces
-  rest a
-  where
-    rest left =
-      ( do
-          s <- char '*' <|> char '/'
-          spaces
-          b <- subExpression <|> val
-          rest $ case s of
-            '*' -> Mul left b
-            '/' -> Div left b
-            _ -> left
-      )
-        <|> return left
-
-subExpression :: Parser Expression
-subExpression = do
-  char '('
-  e <- expression
-  char ')'
-  return e
-
-expression :: Parser Expression
-expression = spaces >> termSum
-
-eval :: Expression -> Int
-eval e = case e of
-  Val x -> x
-  Sum x y -> eval x + eval y
-  Sub x y -> eval x - eval y
-  Mul x y -> eval x * eval y
-  Div x y -> eval x `div` eval y
-
-evalQ :: Expression -> MyRational
-evalQ e = case e of
-  Val x -> Q x 1
-  Sum x y -> evalQ x + evalQ y
-  Sub x y -> evalQ x - evalQ y
-  Mul x y -> evalQ x * evalQ y
-  Div x y -> evalQ x `qdiv` evalQ y
-
-calculator :: String -> Either ParseError Int
-calculator x = do
-  e <- parse expression "(calc)" x
-  return $ eval e
-
-calculatorQ :: String -> Either ParseError MyRational
-calculatorQ x = do
-  e <- parse expression "(calc)" x
-  return $ evalQ e
-
-data Expression = Val Int | Sum Expression Expression | Sub Expression Expression | Mul Expression Expression | Div Expression Expression deriving (Show, Eq)
+-- データ型定義
+data Expression
+  = Val Int
+  | Sum Expression Expression
+  | Sub Expression Expression
+  | Mul Expression Expression
+  | Div Expression Expression
+  deriving (Show, Eq)
 
 data MyRational = Q Int Int deriving (Eq, Show)
 
-instance Num MyRational where
-  Q x1 y1 + Q x2 y2 = Q (x1 * s1 + x2 * s2) (g * s1 * s2)
-    where
-      g = myGCD y1 y2
-      s1 = y2 `div` g
-      s2 = y1 `div` g
+-- 数値のパーサー
+parseValue :: Parser Expression
+parseValue = do
+  digits <- many1 digit
+  return $ Val (read digits :: Int)
 
-qdiv (Q x1 y1) (Q x2 y2) = Q (x1 * y2) (x2 * y1)
+-- 括弧内の式をパースする関数
+parseSubExpression :: Parser Expression
+parseSubExpression = do
+  char '('
+  expr <- expression
+  char ')'
+  return expr
 
--- x = 12
--- y = 18
-myGCD :: Int -> Int -> Int
-myGCD x y = case q of
-  0 -> r
-  _ -> myGCD q r
+-- 掛け算・割り算を含む項のパーサー
+parseFactor :: Parser Expression
+parseFactor = do
+  firstTerm <- parseSubExpression <|> parseValue
+  spaces
+  parseFactorRest firstTerm
   where
-    (q, r) =
-      if x > y
-        then (x `mod` y, y)
-        else (y `mod` x, x)
+    parseFactorRest leftExpr =
+      ( do
+          operator <- char '*' <|> char '/'
+          spaces
+          rightTerm <- parseSubExpression <|> parseValue
+          let newExpr = case operator of
+                '*' -> Mul leftExpr rightTerm
+                '/' -> Div leftExpr rightTerm
+          parseFactorRest newExpr
+      )
+        <|> return leftExpr
+
+-- 足し算・引き算を含む式のパーサー
+parseSum :: Parser Expression
+parseSum = do
+  firstTerm <- parseFactor
+  spaces
+  parseSumRest firstTerm
+  where
+    parseSumRest leftExpr =
+      ( do
+          operator <- char '+' <|> char '-'
+          spaces
+          rightTerm <- parseFactor
+          let newExpr = case operator of
+                '+' -> Sum leftExpr rightTerm
+                '-' -> Sub leftExpr rightTerm
+          parseSumRest newExpr
+      )
+        <|> return leftExpr
+
+-- 式のパーサー
+expression :: Parser Expression
+expression = spaces >> parseSum
+
+-- 整数での評価
+evaluate :: Expression -> Int
+evaluate expr = case expr of
+  Val n -> n
+  Sum x y -> evaluate x + evaluate y
+  Sub x y -> evaluate x - evaluate y
+  Mul x y -> evaluate x * evaluate y
+  Div x y -> evaluate x `div` evaluate y
+
+-- 分数での評価
+evaluateRational :: Expression -> MyRational
+evaluateRational expr = case expr of
+  Val n -> Q n 1
+  Sum x y -> evaluateRational x + evaluateRational y
+  Sub x y -> evaluateRational x - evaluateRational y
+  Mul x y -> evaluateRational x * evaluateRational y
+  Div x y -> evaluateRational x `qdiv` evaluateRational y
+
+-- 整数計算機
+calculator :: String -> Either ParseError Int
+calculator input = do
+  expr <- parse expression "(calc)" input
+  return $ evaluate expr
+
+-- 分数計算機
+calculatorQ :: String -> Either ParseError MyRational
+calculatorQ input = do
+  expr <- parse expression "(calc)" input
+  return $ evaluateRational expr
+
+-- MyRational 型の演算
+instance Num MyRational where
+  Q n1 d1 + Q n2 d2 = Q (n1 * d2 + n2 * d1) (d1 * d2)
+  Q n1 d1 * Q n2 d2 = Q (n1 * n2) (d1 * d2)
+
+-- 分数の除算
+qdiv :: MyRational -> MyRational -> MyRational
+qdiv (Q n1 d1) (Q n2 d2) = Q (n1 * d2) (n2 * d1)
+
+-- 最大公約数 (GCD)
+myGCD :: Int -> Int -> Int
+myGCD x 0 = abs x
+myGCD x y = myGCD y (x `mod` y)
