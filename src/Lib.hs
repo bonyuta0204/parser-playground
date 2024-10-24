bd45555d5f795053ff1f7c6ad666a5384db27273
Yuta Nakamura <nakamurayuta0204@gmail.com>
2024-10-25 00:45:42 +0900

    feat: add Schema show

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,4 +3,4 @@ module Lib (module CSV, module HTML, module Calc, module Schema) where
 import Lib.CSV as CSV (csvFile)
 import Lib.Calc as Calc (expression)
 import Lib.HTML as HTML (html)
-import Lib.Schema as Schema (symbol)
+import Lib.Schema as Schema (readExpr)
--- src/Lib/Schema.hs
+++ src/Lib/Schema.hs
@@ -20,7 +20,6 @@ data LispVal
   | Number Integer
   | String String
   | Bool Bool
-  deriving (Show)
 
 parseString :: Parser LispVal
 parseString = do
@@ -69,3 +68,17 @@ parseQuoted = do
   char '\''
   x <- parseExpr
   return $ List [Atom "quote", x]
+
+showVal :: LispVal -> String
+showVal (String contents) = "\"" ++ contents ++ "\""
+showVal (Atom name) = name
+showVal (Number contents) = show contents
+showVal (Bool True) = "#t"
+showVal (Bool False) = "#f"
+showVal (List contents) = "(" ++ unwordsList contents ++ ")"
+showVal (DottedList head tail) = "(" ++ unwordsList head ++ " . " ++ showVal tail ++ ")"
+
+unwordsList :: [LispVal] -> String
+unwordsList = unwords . map showVal
+
+instance Show LispVal where show = showVal
